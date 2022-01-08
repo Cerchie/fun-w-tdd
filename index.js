da712de42e8cbdef4941eeb3a5bd38e848a10726
Lucia Cerchie <luciacerchie@gmail.com>
2022-01-08 12:58:51 -0700

    adds updates

--- index.js
+++ index.js
@@ -15,20 +15,65 @@ const romanNumerals = {
     V: 5,
     I: 1,
 }
+function romanNumerator(romanNumeral) {
+    if (typeof romanNumeral !== 'string') {
+        return 'value must be a string'
+    }
+    const keys = Object.keys(romanNumerals)
 
-describe('Test expectation', () => {
-    it('should test', () => {
+    const isARomanNumeral = (romanNumeral) => {
+        keys.indexOf(romanNumeral) !== -1
+    }
+    const resultOfCheck = romanNumeral.split().every(isARomanNumeral)
+
+    if (resultOfCheck !== true) {
+        return 'value must be a Roman Numeral'
+    }
+}
+
+describe('roman numeral function works', () => {
+    it('should only take in strings', () => {
         const stub = sinon.stub()
-        stub.returns(['returnedval'])
-
+        stub.returns('value must be a string')
+        const effect = romanNumerator(1)
         assert.equals(stub(), effect)
 
         stub.resetBehavior()
 
         assert.isUndefined(stub())
     })
-    it('should test', () => {})
-    it('should test', () => {})
+    it('should only take in Roman Numerals', () => {
+        const stub = sinon.stub()
+        stub.returns('value must be a Roman Numeral')
+        const effect = romanNumerator('XX')
+        assert.equals(stub(), effect)
+
+        stub.resetBehavior()
+
+        assert.isUndefined(stub())
+    })
+    it('should not fail with multiple Roman characters', () => {
+        const stub = sinon.stub()
+        stub.returns('value must be a Roman Numeral')
+        const effect = romanNumerator('XX')
+        assert.equals(stub(), effect)
+
+        stub.resetBehavior()
+
+        assert.isUndefined(stub())
+    })
+    it('should have a parameter with roman numerals in the right order', () => {
+        const stub = sinon.stub()
+        stub.returns(
+            'value must be a Roman Numeral with numerals in the right order'
+        )
+        const effect = romanNumerator('IXII')
+        assert.equals(stub(), effect)
+
+        stub.resetBehavior()
+
+        assert.isUndefined(stub())
+    })
 })
 
 // https://martinfowler.com/bliki/TestDrivenDevelopment.html
